7f162b45834e7064befae08f13077eba678f86ff
Abdiel Corro <acorrop@outlook.com>
2022-10-11 08:42:01 -0500

    created a temperature conversion calculator

--- StaticExercise/Program.cs
+++ StaticExercise/Program.cs
@@ -4,10 +4,17 @@
     {
         static void Main(string[] args)
         {
-            
-            Console.WriteLine(Math.Floor(TempConverter.FahrenheitToCelsius(90)));
+            Console.WriteLine($"Temperature Conversion Calculator \nEnter a value to convert in Fahrenheit degrees: ");
+            double fahrenheit = double.Parse(Console.ReadLine());
+            fahrenheit = Math.Floor(TempConverter.FahrenheitToCelsius(fahrenheit));
+            Console.WriteLine($"The temperature is {fahrenheit} Fahrenheit degrees");
 
-            Console.WriteLine(Math.Floor(TempConverter.CelsiusToFahrenheit(32)));
+
+            Console.WriteLine($"Enter a value to convert in Celsius degrees: ");
+            double celsius = double.Parse(Console.ReadLine());
+            fahrenheit = Math.Floor(TempConverter.FahrenheitToCelsius(celsius));
+            Console.WriteLine($"The temperature is {celsius} Celsius degrees");
+
         }
     }
 }
